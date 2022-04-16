173b45ea78f3e3fd30ee1fbedbaac493cd2abf7b
Jung Gyu Yoon <yjg30737@gmail.com>
2022-04-16 11:17:15 +0900

    Make "var" directory and move variables.scss in it to not convert "variable.scss"

--- qt_sass_theme_files/qt_theme.py
+++ qt_sass_theme_files/qt_theme.py
@@ -3,6 +3,6 @@ import os
 import qtsass
 
 
-def get_theme(theme='darkgray', output_dir=os.getcwd()):
+def get_theme(output_dir = os.getcwd()):
     dirname = os.path.dirname(__file__)
     qtsass.compile_dirname(dirname, output_dir)
