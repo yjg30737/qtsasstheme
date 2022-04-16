8e1adf2c4a526ed1988c1306559c9f1d217f4720
Jung Gyu Yoon <yjg30737@gmail.com>
2022-04-16 10:30:12 +0900

    First commit from local

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='qt-sass-theme-files',
+    version='0.0.1',
+    author='Jung Gyu Yoon',
+    author_email='yjg30737@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    package_data={'qt_sass_theme_files': ['theme.scss',
+                                          'icon_button.scss',
+                                          'icon_text_button.scss',
+                                          'menu_bar.scss',
+                                          'main_widget.scss',
+                                          'variables.scss']},
+    description='Qt sass theme files',
+    url='https://github.com/yjg30737/qt-sass-theme-files.git'
+)
