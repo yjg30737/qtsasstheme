f39a74e6d450e14cd5e1a61286be02d075d03d2a
Jung Gyu Yoon <yjg30737@gmail.com>
2022-04-16 11:11:00 +0900

    Modify getTheme function

--- qt_sass_theme_files/qt_theme.py
+++ qt_sass_theme_files/qt_theme.py
@@ -3,6 +3,6 @@ import os
 import qtsass
 
 
-def get_theme():
+def get_theme(theme='darkgray', output_dir=os.getcwd()):
     dirname = os.path.dirname(__file__)
-    qtsass.compile_dirname(dirname, os.getcwd())
+    qtsass.compile_dirname(dirname, output_dir)
