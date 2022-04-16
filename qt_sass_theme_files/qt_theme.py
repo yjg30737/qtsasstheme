e83174525b7ef78fda66b1a49d9e50777cf86e29
Jung Gyu Yoon <yjg30737@gmail.com>
2022-04-16 11:27:29 +0900

    Move all scss files into sass directory

--- qt_sass_theme_files/qt_theme.py
+++ qt_sass_theme_files/qt_theme.py
@@ -4,5 +4,5 @@ import qtsass
 
 
 def get_theme(output_dir = os.getcwd()):
-    dirname = os.path.dirname(__file__)
+    dirname = os.path.dirname(os.path.join(__file__, 'sass'))
     qtsass.compile_dirname(dirname, output_dir)
