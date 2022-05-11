db3ec22fb36eebde156a5cab76415ee3a603fe6c
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-11 18:42:39 +0900

    Update setup.py to upload the package to pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='qt-sass-theme-getter',
@@ -16,6 +23,8 @@ setup(
                   },
     description='Qt sass theme getter',
     url='https://github.com/yjg30737/qt-sass-theme-getter.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'qtsass'
     ]
